096ae64885e2dc95c35cf9c7b7f13e431be9ca09
abdullah-pk <abdullah@primekeeper.my>
2017-12-21 12:36:42 +0800

    Added answer

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const claypot = (from, to) => {
-    console.log("Finish me!")
+    for(let i=from; i<to; i++) {
+        if(i%3===0 && i%5===0) {
+            console.log('Claypot');
+        } else if (i%3===0) {
+            console.log('Clay');
+        } else if(i%5===0) {
+            console.log('pot');
+        } else {
+            console.log(i);
+        }
+    }
 }
